52b2eea7cc6b78a295448c29ed2af2da1724c5b4
Fetigue <bemen77@gmail.com>
2017-07-20 12:59:55 -0400

    Update WhenYouCreateACell.java

--- gameoflife-core/src/test/java/com/wakaleo/gameoflife/domain/WhenYouCreateACell.java
+++ gameoflife-core/src/test/java/com/wakaleo/gameoflife/domain/WhenYouCreateACell.java
@@ -1,7 +1,7 @@
 package com.wakaleo.gameoflife.domain;
 
 import static com.wakaleo.gameoflife.domain.Cell.DEAD_CELL;
-import static com.wakaleo.gameoflife.domain.Cell.LIVE_CELL;
+import static com.wakaleo.gameoflife.domain.Cell.LIVE_CELL("+");
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
